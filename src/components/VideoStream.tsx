0d47d3c086e71664b854a8c83fc887b9c20b71cb
fbenaglia <fbenaglia@hotmail.com>
2025-03-21 01:01:01 -0300

    2 channels

--- src/components/VideoStream.tsx
+++ src/components/VideoStream.tsx
@@ -1,13 +1,24 @@
 import ReactPlayer from 'react-player';
 import './VideoStream.css';
-const URL_VIDEO_SERVER = import.meta.env.VITE_URL_VIDEO_SERVER;
+const URL_VIDEO_SERVER = import.meta.env.VITE_URL_VIDEO_SERVER; //NODE VERSION
+
+const URL_VIDEO_SERVER_B = import.meta.env.VITE_URL_VIDEO_SERVER_B; // JAVA VERSION
 //https://www.npmjs.com/package/react-player
 const VideoStream: React.FC = () => {
     return (
         <div id="container">
             <ReactPlayer
                 url={
-                    URL_VIDEO_SERVER + "/video"
+                    //URL_VIDEO_SERVER + "/video"
+                    URL_VIDEO_SERVER_B + "/videos/stream"
+                }
+                controls
+                autoPlay={true}>
+
+            </ReactPlayer>
+            <ReactPlayer
+                url={
+                    URL_VIDEO_SERVER_B + "/video"
                 }
                 controls
                 autoPlay={true}>
